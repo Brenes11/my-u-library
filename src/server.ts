ca46b86c009a7fe3adcb1c795630b635c9987d07
iBrenes <ismael.latin18@itca.edu.sv>
2024-08-15 14:15:16 -0600

    creating principal file to execute server

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/database';
+import userRoutes from './routes/userRoutes';
+import bookRoutes from './routes/bookRoutes';
+import checkoutRoutes from './routes/checkoutRoutes';
+
+dotenv.config();
+connectDB();
+const app = express();
+app.use(express.json());
+
+
+app.use('/api/users', userRoutes);
+app.use('/api/books', bookRoutes);
+app.use('/api/checkouts', checkoutRoutes);
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
