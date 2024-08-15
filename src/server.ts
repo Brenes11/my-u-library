745277617eb8905eafcd0918498e40ed26591a87
iBrenes <ismael.latin18@itca.edu.sv>
2024-08-15 14:22:14 -0600

    adding user route

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,6 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/database';
 import userRoutes from './routes/userRoutes';
-import bookRoutes from './routes/bookRoutes';
-import checkoutRoutes from './routes/checkoutRoutes';
 
 dotenv.config();
 connectDB();
@@ -12,8 +10,7 @@ app.use(express.json());
 
 
 app.use('/api/users', userRoutes);
-app.use('/api/books', bookRoutes);
-app.use('/api/checkouts', checkoutRoutes);
+
 
 
 const PORT = process.env.PORT || 3000;
