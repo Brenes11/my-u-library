fc79e4b6f66e665c150a6499c2be1da7dda7ac8e
iBrenes <ismael.latin18@itca.edu.sv>
2024-08-15 15:37:44 -0600

    dding routes for books and checkouts

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/database';
 import userRoutes from './routes/userRoutes';
+import bookRoutes from './routes/bookRoutes';
+import checkoutRoutes from './routes/checkoutRoutes';
 
 dotenv.config();
 connectDB();
@@ -11,6 +13,8 @@ app.use(express.json());
 
 app.use('/api/users', userRoutes);
 
+app.use('/api/books', bookRoutes);
+app.use('/api/checkouts', checkoutRoutes);
 
 
 const PORT = process.env.PORT || 3000;
