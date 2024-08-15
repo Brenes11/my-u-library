ed627c60b20bb04fc2318a9c7121eba0a76c2ce9
iBrenes <ismael.latin18@itca.edu.sv>
2024-08-15 15:36:39 -0600

    adding validation when creating books creating validation so that only users with the role of librarian can add new users

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -3,22 +3,27 @@
 import { Request, Response } from 'express';
 import { createUser, getUserRole, getAllUsers } from '../service/userService';
 
-export const createUserController = async (req: Request, res: Response) => {
-  try {
-    const { first_name, last_name, email, role, user_id } = req.body;
-    
 
-    const newUser = await createUser({ first_name, last_name, email, role });
-    res.status(201).json(newUser);
-  } catch (err) {
-    res.status(500).json({ message: err });
-  }
-};
 
 export const getUsersController = async (req: Request, res: Response) => {
   try {
     const users = await getAllUsers();
     res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
+
+export const createUserController = async (req: Request, res: Response) => {
+  try {
+    const { first_name, last_name, email, role, user_id } = req.body;
+    const userRole = await getUserRole(user_id);
+    if (userRole !== 'Librarian') {
+      return res.status(403).send({ message: 'Forbidden' });
+    }
+
+    const newUser = await createUser({ first_name, last_name, email, role });
+    res.status(201).json(newUser);
   } catch (err) {
     res.status(500).json({ message: err });
   }
